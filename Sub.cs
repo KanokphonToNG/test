dab7cc00a332bfbbaf7c88f8a83c995e85cfaa82
KanokphonToNG <kanokphon.l@kkumail.com>
2026-03-17 12:22:20 +0700

    Add files via upload

---
+++ Sub.cs
@@ -0,0 +1,24 @@
+﻿class Subject
+    {
+        public string SubjectName { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Subject(string name)
+        {
+            SubjectName = name;
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student s)
+        {
+            Students.Add(s);
+        }
+
+        public void ShowStats()
+        {
+            if (Students.Count == 0) return;
+
+            Console.WriteLine($"Max: {Students.Max(s => s.Score)}");
+            Console.WriteLine($"Min: {Students.Min(s => s.Score)}");
+            Console.WriteLine($"Avg: {Students.Average(s => s.Score):F2}");
+        }
