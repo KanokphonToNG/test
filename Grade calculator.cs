ae07b963b22108b8ce3d9222cac7d32a376b247d
KanokphonToNG <kanokphon.l@kkumail.com>
2026-03-17 12:25:03 +0700

    Add files via upload

---
+++ Grade calculator.cs
@@ -0,0 +1,14 @@
+﻿class GradeCalculator
+{
+    public static string CalculateGrade(double score)
+    {
+        if (score >= 80) return "A";
+        else if (score >= 75) return "B+";
+        else if (score >= 70) return "B";
+        else if (score >= 65) return "C+";
+        else if (score >= 60) return "C";
+        else if (score >= 55) return "D+";
+        else if (score >= 50) return "D";
+        else return "F";
+    }
+}
