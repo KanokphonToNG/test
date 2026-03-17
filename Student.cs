37b950325e3ad6a0669f0c3c4069228b19b9f2ff
KanokphonToNG <kanokphon.l@kkumail.com>
2026-03-17 12:11:12 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿class Student
+{
+    private double score;
+
+    public double Score
+    {
+        get { return score; }
+        set
+        {
+            if (value >= 0 && value <= 100)
+                score = value;
+            else
+                Console.WriteLine("Invalid score!");
+        }
+    }
+}
