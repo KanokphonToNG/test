eb4f6be8873c654df8ee10c18b4c73271b3cd94e
KanokphonToNG <kanokphon.l@kkumail.com>
2026-03-17 12:13:44 +0700

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,27 @@
+﻿class Parent
+{
+    // สิ่งที่ให้ลูกใช้
+}
+
+class Child : Parent
+{
+    // สืบทอดจาก Parent
+}
+class Person
+{
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
+class Student : Person
+{
+    public string StudentID { get; set; }
+
+    public Student(string id, string name) : base(name)
+    {
+        StudentID = id;
+    }
+}
